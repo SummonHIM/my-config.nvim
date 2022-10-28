03e214f77a7d174ca6564b69503b57691ef8f02a
SummonHIM <2239760702@qq.com>
2022-10-28 22:32:59 +0800

    add tab to confirm cmp

--- lua/lsp/nvim-cmp.lua
+++ lua/lsp/nvim-cmp.lua
@@ -21,7 +21,10 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({
             select = true
-        }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<Tab>'] = cmp.mapping.confirm({
+            select = true
+        })
     }),
     sources = cmp.config.sources({{
         name = 'nvim_lsp'
