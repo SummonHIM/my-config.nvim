ca88da3d7740af421fe58335651cbadf1e99a7b3
SummonHIM <2239760702@qq.com>
2022-12-20 19:23:34 +0800

    load plugins first

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,18 @@
-require('keymaps')
+vim.cmd([[
+    set number
+    colorscheme material
+    set completeopt=menu,menuone,noselect
+    set list
+    set tabstop=4
+    set softtabstop=4
+    set shiftwidth=4
+    set autoindent
+    set smartindent
+    set nowrap
+]])
+
 require('plugins')
+require('keymaps')
 require('lsp/nvim-cmp')
 require('lsp/nvim-lspconfig')
 vim.g.material_style = "darker"
@@ -14,16 +27,3 @@ require('lualine').setup {
 require('tools/nvim-tree')
 require("dapui").setup()
 require("tools/nvim-dap")
-
-vim.cmd([[
-    set number
-    colorscheme material
-    set completeopt=menu,menuone,noselect
-    set list
-    set tabstop=4
-    set softtabstop=4
-    set shiftwidth=4
-    set autoindent
-    set smartindent
-    set nowrap
-]])
